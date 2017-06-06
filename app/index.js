a98022b5544c17a671f1421c53e55ed29f74deeb
jdu <jdu.xian@gmail.com>
2017-06-06 11:26:36 +0800

    add webpack config

--- app/index.js
+++ app/index.js
@@ -1,9 +1,9 @@
 import _ from 'lodash';
 
 function component () {
-  var element = document.createElement('div');
+  var element = document.createElement('section');
 
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
+  element.innerHTML = _.join(['Hello','World'], ' ');
 
   return element;
 }
